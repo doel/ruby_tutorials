3c70bf671d736d83c560c7d95c904f8ff6ecbc31
doelsengupta <doels11@gmail.com>
2015-07-23 18:52:49 -0500

    with inheritance

--- macro.rb
+++ macro.rb
@@ -1,18 +1,24 @@
-class Movie
-
+#https://pragmaticstudio.com/tutorials/ruby-macros
+module ActiveRecord
+  class Base
 	def self.has_many(name)
-	  puts "within has_many #{name}"
-	
+          puts "within has_many #{name}"
+
 
          define_method(name) do
-	  puts "within define method parameter #{name}"
-	end
-
-	end 
-
-has_many("doel")
+          	puts "within define method parameter #{name}"
+	        puts "Select * from doels WHERE..."
+		[]
+          end
+        end
+  end
 end
 
+class Movie < ActiveRecord::Base
+end 
+
+Movie.has_many("doels")
+
 m=Movie.new
-m.doel
+m.doels
 
